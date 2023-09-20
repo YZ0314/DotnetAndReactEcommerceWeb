f301bec381a57ffc5aa5d8442043174f7101849f
yhee0065 <yhee0065@student.monash.edu>
2023-09-21 01:38:23 +1000

    create react app(localhost 3000)

---
+++ ReStore/client/src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+
+export default function App() {
+  const[products,setProducts]=useState();
+
+  /**
+   * fetch the products dtat from .net web api 
+   */
+  useEffect(()=>{
+    fetch('http://localhost:5000/api/products')
+    .then(Response=>Response.json())
+    .then(data=>setProducts(data))
+  })
+
+
+  return (
+    <div>
+      <h1>
+        Re-Store
+      </h1>
+    </div>
+    
+  )
+}
