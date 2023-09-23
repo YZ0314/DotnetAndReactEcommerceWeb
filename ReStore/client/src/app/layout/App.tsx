29edbd09cd78f47a4c6826e59f112c5684d5b8f4
yhee0065 <yhee0065@student.monash.edu>
2023-09-24 04:12:21 +1000

    add switch bar to change the whole theme style

--- ReStore/client/src/app/layout/App.tsx
+++ ReStore/client/src/app/layout/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Product } from '../models/product';
 import Catalog from '../../features/Catalog/Catalog';
-import { Container, CssBaseline, ThemeProvider, Typography, createTheme } from '@mui/material';
+import { Container, CssBaseline, Switch, ThemeProvider, Typography, createTheme } from '@mui/material';
 import Header from '../../features/Catalog/Header';
 
 
@@ -16,11 +16,15 @@ export default function App() {
     mode:palettetype
   }
  })
- 
+
+ function changeTheme(){
+  setDarkMode(!darkMode)
+ }
+
   return (
     <ThemeProvider theme={theme}>
     <CssBaseline/>
-     <Header></Header>
+     <Header darkMode={darkMode} changeTheme={changeTheme}></Header>
      <Container>
       <Catalog/>
     </Container>
--- ReStore/client/src/features/Catalog/Header.tsx
+++ ReStore/client/src/features/Catalog/Header.tsx
@@ -1,12 +1,22 @@
-import { AppBar, Toolbar,Typography } from '@mui/material'
+import { AppBar, Switch, Toolbar,Typography } from '@mui/material'
 import React from 'react'
+import SwitchTheme from './SwitchTheme'
 
-export default function Header() {
+interface Props{
+  changeTheme:()=>void;
+  darkMode:boolean;
+
+}
+
+export default function Header({changeTheme,darkMode}:Props) {
   return (
     <AppBar position='static' sx={{mb:4}}>
       <Toolbar>
         <Typography variant='h6'> RE-STORE </Typography>
+        <SwitchTheme darkMode={darkMode} changeTheme={changeTheme}></SwitchTheme>
       </Toolbar>
+
+      
     </AppBar>
   )
 }
