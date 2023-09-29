7559ad7046f00703ea45cfacb8a71aff713dbc26
yhee0065 <yhee0065@student.monash.edu>
2023-09-29 18:10:03 +1000

    fetching the basket when app start, use react context to centrlise the basket state

--- ReStore/client/src/app/layout/App.tsx
+++ ReStore/client/src/app/layout/App.tsx
@@ -6,11 +6,29 @@ import Header from '../../features/Catalog/Header';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
+import { useStoreContext } from '../context/StoreContext';
+import { getCookie } from '../util/util';
+import agent from '../api/agent';
+import { error } from 'console';
+import LoadingComponent from './LoadingComponent';
 
 
 
 
 export default function App() {
+  const{setBasket}=useStoreContext();
+  const[loading,setLoading]=useState(true);
+
+  useEffect(()=>{
+    const buyerId=getCookie('buyerId');
+    if (buyerId){
+      agent.Basket.get()
+      .then(basket=>setBasket(basket))
+      .catch(error=>console.log(error))
+      .finally(()=>setLoading(false))
+    }
+  },[setBasket])
+
   const [darkMode,setDarkMode]=useState(false);
   const palettetype=darkMode?'dark':'light';
  const theme=createTheme({
@@ -22,6 +40,8 @@ export default function App() {
  function changeTheme(){
   setDarkMode(!darkMode)
  }
+
+ if(loading) return<LoadingComponent message='Initialising App...'/>
 
   return (
     <ThemeProvider theme={theme}>
