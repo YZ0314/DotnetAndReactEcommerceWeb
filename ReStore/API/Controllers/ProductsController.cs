4c4782fa70b3020e39ed93054447223d28d7d975
yhee0065 <yhee0065@student.monash.edu>
2023-10-24 17:06:50 +1100

    get unique brands and types as list

--- ReStore/API/Controllers/ProductsController.cs
+++ ReStore/API/Controllers/ProductsController.cs
@@ -1,46 +1,62 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using API.Extensions;
+using API.RequestHelper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    
-    public class ProductsController: BaseApiController
+
+    public class ProductsController : BaseApiController
     {
-        
+
         private readonly StoreContext _context;
-        
+
         public ProductsController(StoreContext context)
         {
             this._context = context;
-           
-            
+
+
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy,string searchTerm,string brands,String types){
-            var query=  _context.Products
-            .Sort(orderBy)
-            .Search(searchTerm)
-            .Filter(brands,types)
+        public async Task<ActionResult<List<Product>>> GetProducts([FromQuery]ProductParams productParams)
+        {
+            var query = _context.Products
+            .Sort(productParams.OrderBy)
+            .Search(productParams.searchTerm)
+            .Filter(productParams.Brands, productParams.Types)
             .AsQueryable();
-            return await query.ToListAsync();
+        var products = await PagedList<Product>.ToPagedList(query,productParams.PageNumber,productParams.PageSize);
+
+        // Response.Headers.Add("Pageination", JsonSerializer.Serialize(products.MetaData));
+        Response.AddPaginationHeader(products.MetaData);
+
+        return products;
 
 
-           
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id){
-            var Product= await _context.Products.FindAsync(id);
-            if(Product==null) return NotFound();
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var Product = await _context.Products.FindAsync(id);
+            if (Product == null) return NotFound();
             return Product;
+        }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters(){
+            var brands=await _context.Products.Select(p=>p.Brand).Distinct().ToListAsync();
+            var types=await _context.Products.Select(p=>p.Type).Distinct().ToListAsync();
+
+            return Ok(new {brands,types});
         }
     }
 }
---
+++ ReStore/API/RequestHelper/ProductParams.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.RequestHelper
+{
+    public class ProductParams :PaginationParams
+    {
+        public string OrderBy { get; set; }
+        public string searchTerm { get; set; }
+        public string Types { get; set; }
+
+        public string Brands { get; set; }
+
+    }
+}
