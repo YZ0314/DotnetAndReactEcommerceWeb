50002426f588e511cac8830eebeb484ed8818333
yhee0065 <yhee0065@student.monash.edu>
2023-11-04 21:50:50 +1100

    add skeleton to product card to clearn up the loading

--- ReStore/client/src/features/Catalog/ProductList.tsx
+++ ReStore/client/src/features/Catalog/ProductList.tsx
@@ -2,16 +2,26 @@ import { Avatar, Grid, List, ListItem, ListItemAvatar, ListItemText } from '@mui
 import React from 'react'
 import { Product } from '../../app/models/product'
 import ProductCard from './ProductCard'
+import { useAppSelector } from '../../app/store/configureStore'
+import ProductCardSkeleton from './ProductCardSkeleton'
  
 interface Props{
     products:Product[]
 }
 export default function ProductList({products}:Props) {
+  const{productsLoaded} =useAppSelector(state=> state.catalog)
   return (
     <div>
          <Grid container spacing={4}>
-                {products.map((products) => (
-                    <Grid item xs={4}><ProductCard key={products.id} products={products}></ProductCard></Grid>
+                {products.map((products) =>(
+                          
+                    <Grid item xs={4} key={products.id}>
+                      {!productsLoaded ?(
+                        <ProductCardSkeleton/> 
+                      ):( <ProductCard key={products.id} products={products}></ProductCard>)
+                       
+                      }
+</Grid>
                 ))}
             </Grid>
     </div>
