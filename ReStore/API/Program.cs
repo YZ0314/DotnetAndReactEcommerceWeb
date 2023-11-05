e907195a853e57b356e1bdf6f6f5961e55567d82
yhee0065 <yhee0065@student.monash.edu>
2023-11-06 01:45:47 +1100

    use aspnetcore identity to set up the login and register function

--- ReStore/API/Program.cs
+++ ReStore/API/Program.cs
@@ -1,6 +1,8 @@
 using API.Data;
+using API.Entities;
 using API.Middleware;
 using API.Service;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +17,14 @@ builder.Services.AddDbContext<StoreContext>(opt=>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddIdentityCore<User>(opt=>
+{
+    opt.User.RequireUniqueEmail=true;
+})
+.AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<StoreContext>();
+
+builder.Services.AddAuthentication();
 builder.Services.AddScoped<ProductService>();
 var app = builder.Build();
 
@@ -36,16 +46,17 @@ app.MapControllers();
 
 var scope =app.Services.CreateScope();
 var context=scope.ServiceProvider.GetRequiredService<StoreContext>();
+var userManager=scope.ServiceProvider.GetRequiredService<UserManager<User>>();
 var logger=scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
 try
 {
-    context.Database.Migrate();
-    DbInitializer.Initialize(context);
+   await  context.Database.MigrateAsync();
+   await DbInitializer.Initialize(context,userManager);
 }
 catch (Exception ex)
 {
     logger.LogError(ex,"A problem occurred during migration");
-    throw;
+    
 }
 
 app.Run();
